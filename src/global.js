fef84d602c7e743e5d4858750a66aa2988ac0eb7
jemba narcisse <cabliveer@gmail.com>
2023-06-29 21:08:01 +0200

    Add global file

---
+++ src/global.js
@@ -0,0 +1,3 @@
+const BASE_URL = 'https://restcountries.com/v3.1/region/europe';
+export const BASE_URL1 = 'https://countriesnow.space/api/v0.1/countries/population/cities/filter';
+export default BASE_URL;
